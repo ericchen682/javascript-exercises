1550f6973b34c6f9d36b2bbf8242f51b7838163d
Eric Chen <eric.chen682@gmail.com>
2025-12-18 16:20:17 -0800

    Complete js foundations exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof a != "number" || typeof b != "number" || a <= 0 || b <= 0 || a%1 != 0 || b%1 != 0) return "ERROR";
+    return (a+b)*(Math.abs(a-b)+1)/2;
 };
 
 // Do not edit below this line
