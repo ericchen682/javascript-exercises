2f2c2a4955b6e14563efa7d9cbd271810e5e6052
Eric Chen <eric.chen682@gmail.com>
2025-12-18 16:26:16 -0800

    Complete js foundations exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp -= 32;
+  temp *= 5/9;
+  temp = Math.round(temp*10)/10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp *= 9/5;
+  temp += 32;
+  temp = Math.round(temp*10)/10;
+  return temp;
 };
 
 // Do not edit below this line
